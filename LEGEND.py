742959742321a80b70111f7d771db0fa644e9150
Rahul <bgmiddos123451@gmail.com>
2024-12-20 02:21:15 +0530

    Update LEGEND.py

--- LEGEND.py
+++ LEGEND.py
@@ -21,9 +21,9 @@ redeem_codes_collection = db['redeem_codes']
 attack_logs_collection = db['user_attack_logs']
 
 # Bot Configuration
-TELEGRAM_BOT_TOKEN = '6387383740:AAEtryZ_A2IiijIcEIjZfvPx_KE9PIdSw6c'
-ADMIN_USER_ID = 7372592479 
-ADMIN_USER_ID = 1441704343 
+TELEGRAM_BOT_TOKEN = '7403028364:AAEzgUYQkiSylyNs9rc0v9FyNs7QpUzmlac'
+ADMIN_USER_ID = 6778559612 
+ADMIN_USER_ID = 6778559612 
 COOLDOWN_PERIOD = timedelta(minutes=1) 
 user_last_attack_time = {} 
 user_attack_history = {}
